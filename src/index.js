0b0283014898cca3ceb8d217d15405051e0c0549
Paige Tiedeman <paigetiedeman@gmail.com>
2021-11-09 11:06:25 -0800

    Two players working
    Co-Authored-By: chansen1395 <chansen13@georgefox.edu>

--- src/index.js
+++ src/index.js
@@ -35,6 +35,33 @@ $(document).ready(function() {
     const newState = stateControl(shade);
     $('#light-value').text(`Light: ${newState.light}`);
   });
+  $('#feed2').click(function() {
+    const newState = stateControl(feed);
+    $('#soil-value2').text(`Soil: ${newState.soil}`);
+  });
+  
+  $('#super-feed2').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value2').text(`Soil: ${newState.soil}`);
+  });
+
+  $('#water2').click(function() {
+    const newState = stateControl(hydrate);
+    $('#water-value2').text(`Water: ${newState.water}`);
+  });
+  $('#super-water2').click(function() {
+    const newState = stateControl(superWater);
+    $('#water-value2').text(`Water: ${newState.water}`);
+  });
+  
+  $('#light2').click(function() {
+    const newState = stateControl(light);
+    $('#light-value2').text(`Light: ${newState.light}`);
+  });
+  $('#shade2').click(function() {
+    const newState = stateControl(shade);
+    $('#light-value2').text(`Light: ${newState.light}`);
+  });
 
   // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
 
@@ -43,5 +70,20 @@ $(document).ready(function() {
     const currentState = stateControl();
     $('#soil-value').text(`Soil: ${currentState.soil}`);
     $('#water-value').text(`Water: ${currentState.water}`);
+  });
+
+  $("#form-name").submit(function (event) {
+    event.preventDefault();
+    let inputName = $("#name").val();
+    $("#name-display").text(` ${inputName}`);
+    $(".name-results").show();
+    $("#form-name").hide();
+  });
+  $("#form-name2").submit(function (event) {
+    event.preventDefault();
+    let inputName = $("#name2").val();
+    $("#name-display2").text(` ${inputName}`);
+    $(".name-results2").show();
+    $("#form-name2").hide();
   });
 });
